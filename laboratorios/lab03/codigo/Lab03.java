9f78823a5714798b3b66a1be23d2843daffb94c9
eafit-201710076010 <30275940+eafit-201710076010@users.noreply.github.com>
2018-03-11 03:24:22 -0500

    1.4

--- laboratorios/lab03/codigo/Lab03.java
+++ laboratorios/lab03/codigo/Lab03.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
+import java.util.*;
 public class Lab03 {    
     //basado en http://codegists.com/snippet/java/nqueensjava_ajayramesh23_java
-    
 
     //PUNTO 1.1
     private static boolean puedoPonerReina(int r, int c, int[] tablero) {
@@ -16,15 +15,13 @@ public class Lab03 {
     }
 
     public static int[] nReinas(int n) {
-        
+
         int[] tablero= new int[n];
         return nReinas(0, tablero.length,tablero) ? tablero  : null;
-       
-        
+
     }
 
     private static boolean nReinas(int r, int n, int[] tablero) {
-
         for (int c = 0; c < n; c++) {
             if (puedoPonerReina(r, c,tablero)) {
                 tablero[r] = c;
@@ -38,12 +35,55 @@ public class Lab03 {
         }
         return false;
     }
-    
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         long startTime = System.nanoTime();
-        System.out.println(Arrays.toString(nReinas(32)));
+        System.out.println(Arrays.toString(nReinas(5)));
         long finalTime = System.nanoTime()-startTime;
         System.out.println(finalTime);
+        Digraph g2 = new DigraphAM(12);
+        g2.addArc(5,11,1);
+        g2.addArc(11,2,1);
+        g2.addArc(7,11,1);
+        g2.addArc(7,8,1);
+        g2.addArc(11,9,1);
+        g2.addArc(11,10,1);
+        g2.addArc(3,8,1);
+        g2.addArc(3,10,1);
+        g2.addArc(8,9,1);
+        System.out.println(bfsRetornador(g2,7));
+
     }
+    //RECORRIDO BFS
+    public static ArrayList<Integer> bfsRetornador(Digraph g, int v){
+        boolean[] vi = new boolean[g.size()];
+        ArrayList<Integer> respuesta = new ArrayList<Integer>();
+        bfsRetornador(g, v, respuesta, vi);
+        return respuesta;
+    }     
+
+    private static void bfsRetornador(Digraph g, int v,ArrayList<Integer> l, boolean[] vi){
+        Queue <Integer> q= new LinkedList<Integer>();
+        vi[v] = true;
+        q.add(v);
+        l.add(v);
+        while(!q.isEmpty()){
+            int actual = q.poll();
+            //vi[actual] = true;
+            ArrayList<Integer>  sucesores = g.getSuccessors(actual);
+            if (sucesores != null){
+                for(Integer sucesor: sucesores){
+
+                    if (!vi[sucesor]) {// vi[sucesor] == false
+                        vi[sucesor] = true;
+                        q.add(sucesor);
+                        l.add(sucesor);}
+                }
+
+            }
+
+        }
+
+    }	
 
 }
--- laboratorios/lab03/codigo/TestsLab3.java
+++ laboratorios/lab03/codigo/TestsLab3.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.*;
 /**
  * The test class TestsLab3.
  *
@@ -39,6 +39,7 @@ public class TestsLab3
     {
     }
 
+    //PUNTO 1.2
     /**
      * Test método nReinas(una solución)
      */
@@ -46,15 +47,56 @@ public class TestsLab3
     public void test1_1(){
         int [] res4=Lab03.nReinas(4);
         int [] esp4={1,3,0,2};
-        assertArrayEquals (res4,esp4);
+        assertArrayEquals (esp4,res4);
 
         int [] res8=Lab03.nReinas(8);
         int [] esp8={0,4,7,5,2,6,1,3};
-        assertArrayEquals (res8,esp8);
+        assertArrayEquals (esp8,res8);
 
         int [] res3=Lab03.nReinas(3);
         int [] esp3=null;
-        assertArrayEquals (res3,esp3);
+        assertArrayEquals (esp3,res3);
+    }
+
+    /**
+     * Test recorrido BSF 
+     */
+    @Test
+    public void test1_3(){
+        Digraph g = new DigraphAL(4);
+        g.addArc(0,1,1);
+        g.addArc(1,3,1);
+        g.addArc(0,2,1);
+        g.addArc(3,0,1);
+        ArrayList<Integer> esp = Lab03.bfsRetornador(g,0);
+        ArrayList<Integer> res= new ArrayList<Integer>();
+        res.add(0);
+        res.add(1);
+        res.add(2);
+        res.add(3);
+
+        assertEquals(esp,res);
+
+        Digraph g2 = new DigraphAM(12);
+        g2.addArc(5,11,1);
+        g2.addArc(11,2,1);
+        g2.addArc(7,11,1);
+        g2.addArc(7,8,1);
+        g2.addArc(11,9,1);
+        g2.addArc(11,10,1);
+        g2.addArc(3,8,1);
+        g2.addArc(3,10,1);
+        g2.addArc(8,9,1);
+        ArrayList<Integer> esp2 = Lab03.bfsRetornador(g2,7);
+        ArrayList<Integer> res2= new ArrayList<Integer>();
+        res2.add(7);
+        res2.add(8);
+        res2.add(11);
+        res2.add(9);
+        res2.add(2);
+        res2.add(10);
+
+        assertEquals(esp2,res2);
     }
 }
 
