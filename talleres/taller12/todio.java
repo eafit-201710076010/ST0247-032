f322b34b0e77b66b2cfd2704c9870a26aee56c22
eafit-201710076010 <30275940+eafit-201710076010@users.noreply.github.com>
2018-05-03 13:38:44 -0500

    Update todio.java

--- talleres/taller12/todio.java
+++ talleres/taller12/todio.java
@@ -1,43 +1,20 @@
-
-/**
- * Write a description of class taller12 here.
- * 
- * @author (your name) 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * @version (a version number or a date)
- */
-
-public class taller12
+public class Todio
 {
-    static int contador=0;
-    private static boolean numeroataque(int r, int c, int[] tablero) {
-        // complete...
+       
+ public static int numeroAtaque(int[] tablero) {
+         int contador=0;
+        for(int i = 0; i < tablero.length; i++)
+            for(int j = i+1; j < tablero.length; j++)
+                if (tablero[i] == tablero[j] || Math.abs(tablero[i] - tablero[j]) == Math.abs(i - j))
+                    contador++; 
+        return contador;
 
-        for (int i = c; i < r; i++) {
-            if (tablero[i] == c || (i - r) == (tablero[i] - c) ||(i - r) == (c - tablero[i])) 
-            {
-                contador++;
-
-            }
-        }
-        return true;
     }
-
+    
     public static void
     taller_12(int nreinas){
         int [] arreglo= new int [nreinas];
-        //   arreglo[1]=1;
-        for(int j=0;j<nreinas;j++)
-            for(int i=0; i<nreinas; i++){
-                numeroataque(j,i,arreglo);
-            }
-        int a=0;
+        int papa= numeroAtaque(arreglo);
     }
 
     public static void aux(int [] papito){
