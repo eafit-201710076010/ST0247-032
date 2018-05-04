9bfbe8eab116b6eb069fd78ceaaba1602de97040
eafit-201710076010 <30275940+eafit-201710076010@users.noreply.github.com>
2018-05-04 11:20:22 -0500

    Add files via upload

--- talleres/taller12/Todio.java
+++ talleres/taller12/Todio.java
@@ -31,28 +31,35 @@ public class Todio
         int [] arreglo= new int [nreinas];
         int papa= numeroAtaque(arreglo);
         int[] nuevo= aux(arreglo,nreinas,papa);
-        while(numeroAtaque(nuevo)<numeroAtaque(arreglo)){
-            arreglo=nuevo;
+        while(numeroAtaque(nuevo)<papa){
+            int j=numeroAtaque(arreglo);
+            int al= numeroAtaque(nuevo);
+            arreglo=nuevo.clone();
             papa=numeroAtaque(arreglo);
             nuevo=aux(arreglo,nreinas,papa);
         }
         return arreglo;
     }
 
-    public static int[] aux(int [] papito,int nreinas,int papa){
-        int [] nuevopapi= new int [nreinas] ;
+    public static int[] aux(int [] padre,int nreinas,int papa){
+        int [] nuevoPadre= padre.clone();
+        int []temp= padre.clone();
         for(int i=0; i<nreinas; i++){
-            
-            for(int j=0; j<nreinas; j++){
-                papito[i]=j;
-                int hijo=numeroAtaque(papito);
-                if(hijo<=papa){
-                    nuevopapi= papito;
-                }
 
+            for(int j=0; j<nreinas; j++){
+                padre[i]=j;
+                int hijo=numeroAtaque(padre);
+                if(hijo<=papa){
+                    int a=0;
+                    nuevoPadre= padre.clone();
+                    papa=hijo;
+                }else{
+                    nuevoPadre=nuevoPadre;
+                }
+                padre[i]=temp[i];
             }
-            
+
         }
-        return nuevopapi;
+        return nuevoPadre;
     }
 }
