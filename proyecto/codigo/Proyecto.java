a1e4a07fdc0bc10578f7515c8b72d59b6a32490c
eafit-201710076010 <30275940+eafit-201710076010@users.noreply.github.com>
2018-05-14 23:34:47 -0500

    Add files via upload

--- proyecto/codigo/Proyecto.java
+++ proyecto/codigo/Proyecto.java
@@ -4,8 +4,7 @@ import java.io.*;
 class Proyecto
 
 {
-    
-     
+
     static double [] losLG;
     static int n=0,m=0, u=0, breaks =0; 
     static Float r=0f, speed=0f, Tmax,Smax, st_customer, Q =0f, Qini =0f;
@@ -52,33 +51,33 @@ class Proyecto
         linea=lector.readLine();
         lineaPartida = linea.split(" ");
         for(int i=0; i<n; i++)   
-         { if(lineaPartida[4].equals("c") || lineaPartida[4].equals("d")){
-            arreglo[i] = new Pair(Float.parseFloat(lineaPartida[2]),Float.parseFloat(lineaPartida[3]));
-            g.AddInfo(i,lineaPartida[1]+""+lineaPartida[5]); 
-            linea=lector.readLine();
-            lineaPartida = linea.split(" ");
-        }else{
-        array.add(new Pair(Float.parseFloat(lineaPartida[2]),Float.parseFloat(lineaPartida[3])));
-        gs.AddInfo(i-321,lineaPartida[1]+lineaPartida[5]);
+        { if(lineaPartida[4].equals("c") || lineaPartida[4].equals("d")){
+                arreglo[i] = new Pair(Float.parseFloat(lineaPartida[2]),Float.parseFloat(lineaPartida[3]));
+                g.AddInfo(i,lineaPartida[1]+""+lineaPartida[5]); 
+                linea=lector.readLine();
+                lineaPartida = linea.split(" ");
+            }else{
+                array.add(new Pair(Float.parseFloat(lineaPartida[2]),Float.parseFloat(lineaPartida[3])));
+                gs.AddInfo(i-(m+1),lineaPartida[1]+lineaPartida[5]);
+            }
         }
-        }
-         
+
         for(int i=0; i<m; i++){
             for(int j=0;j<m;j++){
                 g.addArc(i,j,((float)Math.sqrt(
                             Math.pow(arreglo[i].first-arreglo[j].first,2) +
                             Math.pow(arreglo[i].second-arreglo[j].second,2)
                         )));
-               
+
             }
 
         }
         for(int i=0; i<m; i++){
             for(int j=0;j<array.size();j++){
-             gs.addArc(i,j,(float)Math.sqrt(
-                            Math.pow(arreglo[i].first-array.get(j).first,2) +
-                            Math.pow(arreglo[i].second-array.get(j).second,2)
-                        ));
+                gs.addArc(i,j,(float)Math.sqrt(
+                        Math.pow(arreglo[i].first-array.get(j).first,2) +
+                        Math.pow(arreglo[i].second-array.get(j).second,2)
+                    ));
             }}
         ////////////OJO
         linea=lector.readLine();
@@ -86,7 +85,7 @@ class Proyecto
         linea=lector.readLine();
         lineaPartida = linea.split(" ");
         ///if(lineaPartida[0].equals("l")){
-         losLG= new double[3];
+        losLG= new double[3];
         for(int i=0; i<3;i++){
             //linea=lector.readLine();
             String [] losLpri = linea.split(" ");
@@ -122,7 +121,7 @@ class Proyecto
     public static long time2(){
         long estimatedTime=0;
         try{
-            Digraph g= leer("tc2c320s38ct1.txt");
+            Digraph g= leer("tc2c320s24ct4.txt");
             long startTime = System.currentTimeMillis();
             //AgenteViajero(g);
             estimatedTime= System.currentTimeMillis() - startTime;
@@ -133,7 +132,7 @@ class Proyecto
 
     public static void main(String[] args) throws IOException
     {
-        Digraph g = leer("tc2c320s38ct0.txt");
+        Digraph g = leer("tc2c320s24cf1.txt");
         //Ruta route= new Ruta();
         //route.AgenteViajero(g);
 
@@ -154,9 +153,11 @@ class Proyecto
         int contSC=0;
         float distanciaT=0;
         float pesosucesor=0;
+        int ruta=1;
         boolean[] visitados = new boolean[g.size()];
         ArrayList<Integer> sol=new ArrayList<Integer>();
         for(int j=0;j<g.size()-1;j++){
+            
             int i;
             sol.add(v);
             ArrayList<Integer> sucesores= g.getSuccessors(v);
@@ -168,43 +169,43 @@ class Proyecto
                 if(g.getWeight(v,sucesores.get(i))<pesosucesor && !visitados[sucesores.get(i)]){
                     verticesucesor= sucesores.get(i);
                     pesosucesor=g.getWeight(v,verticesucesor);
-                    
+
                 }    
                 //sol.add(i);
 
             }
             int a=0;
             if (g.getInfo(verticesucesor).contains("c"))
-               { tiempo=time(v,verticesucesor,g)+st_customer;
-                 energ= tiempo* r;
-                 if(Q-energ<=Q*(30/100)){
+            { tiempo=time(v,verticesucesor,g)+st_customer;
+                energ= pesosucesor* r;
+                if(Q-energ<=Q*(30/100)){
                     ArrayList<Integer> sucesoresST= gs.getSuccessors(v);
                     a=sucesoresST.get(0);
                     Float pesoo= gs.getWeight(v,a); 
                     double demora= losLG[Character.digit((g.getInfo(sucesoresST.get(0)))
-                    .charAt(g.getInfo(sucesoresST.get(0)).length()-1),10)];
+                                .charAt(g.getInfo(sucesoresST.get(0)).length()-1),10)];
                     for(int k=1;k<sucesoresST.size(); k++){
-                     Float peso2= gs.getWeight(v,sucesoresST.get(k));
-                     double demora2= losLG[Character.digit((g.getInfo(sucesoresST.get(k))).charAt(g.getInfo(sucesoresST.get(k)).length()-1),10)];
-                    if(peso2+demora2<pesoo+demora){
-                    a=sucesoresST.get(k);
-                    pesoo=peso2;
-                    demora=demora2;
-                    }
+                        Float peso2= gs.getWeight(v,sucesoresST.get(k));
+                        double demora2= losLG[Character.digit((g.getInfo(sucesoresST.get(k))).charAt(g.getInfo(sucesoresST.get(k)).length()-1),10)];
+                        if(peso2+demora2<pesoo+demora){
+                            a=sucesoresST.get(k);
+                            pesoo=peso2;
+                            demora=demora2;
+                        }
                     }
                     verticesucesor=a;
                     pesosucesor=gs.getWeight(v,verticesucesor);
                     tiempo=(float)demora+time(v,a,gs)+st_customer;
-                    energ= tiempo* r;
+                    energ= pesosucesor* r;
                     Q=Qini;
-                    }else{
+                }else{
                     Q=Qini;
-                    }
+                }
             }else{
                 tiempo=time(v,verticesucesor,g);
                 contSC++;
             }
-          
+
             distanciaT=distanciaT+pesosucesor;
             totalT+=tiempo;
             v=verticesucesor;
@@ -212,11 +213,14 @@ class Proyecto
                 sol.add(de);
                 System.out.println(Arrays.toString(sol.toArray()));
                 System.out.println(contSC);
+                System.out.println("Ruta "+ ruta);
                 sol=new ArrayList();
                 v=0;
+                ruta++;
                 totalT=0;
                 contSC=0;
                 i--;
+
             }
 
         }
@@ -225,9 +229,10 @@ class Proyecto
         System.out.println(Arrays.toString(sol.toArray()));
         distanciaT=distanciaT+ g.getWeight(verticesucesor,0);
         totalT+=time(v,0,g);
-
+        System.out.println(ruta);
         return totalT;
     }
+
     public static float time(int prev, int next, Digraph g){
         float time;
         float dist=g.getWeight(prev, next);
